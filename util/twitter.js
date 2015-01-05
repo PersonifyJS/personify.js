98af33bdb4c05634386f10d21a0e623511c27f39
Rohan Agrawal <rohankagrawal@gmail.com>
2015-01-05 15:31:19 -0800

    update from watsonTestApp

--- util/twitter.js
+++ util/twitter.js
@@ -7,7 +7,7 @@ var Watson = function(watsonConfig, twitterConfig) {
 // var w = new Watson({??}   ,  {????})
 
 
-  var twitterData;
+  var twitterData = '';
 
   var T = new Twit({
           consumer_key        :  twitterConfig.consumer_key,
@@ -20,14 +20,14 @@ var Watson = function(watsonConfig, twitterConfig) {
     // `req.body.subject` is the subject that was entered by the end user
     // TODO: to have the end user enter the date
   Watson.prototype.user = function(twitterHandler) {
-    T.get('search/tweets', { q: ''+twitterHandler+ },
+    T.get('statuses/user_timeline', { screen_name: twitterHandler, count: 100 },
                              function(err, data, response) {
-      console.log(data.statuses.length)
-      for(var i = 0; i < data.statuses.length; i++) {
-        // accumulate the data (each tweet as a text) received from twitter
-        twitterData += data.statuses[i].text;
-      }
-      watsonModule.watson(watsonConfig, twitterData);
+                              var stringz = '';
+          for (var i = 0; i < data.length; i++) {
+            twitterData += data[i].text;
+          }
+          console.log(twitterData);
+          watsonModule.watson(watsonConfig, twitterData);
     });
   };
 
