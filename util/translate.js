77e485db27bbdeda8d302bb40add5dc1873b8633
Phil Elauria <phil.elauria@gmail.com>
2015-01-07 19:17:29 -0800

    updated app.js to include three Oauth credentials

--- util/translate.js
+++ util/translate.js
@@ -28,84 +28,84 @@ var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
 //module.exports.translate parameters info
 //language: enus, frfr, arar, ptbr, eses 
 //outputType: txt, json, xml
-var translate = function(authenticate, data, language, outputType ,callback){
+var translate = function(authenticate, data, language, outputType, callback){
 
-// // defaults for dev outside bluemix
-// var service_url = authenticate.watsonConfig.service_url;
-// var service_username = authenticate.watsonConfig.service_username;
-// var service_password = authenticate.watsonConfig.service_password;
+// defaults for dev outside bluemix
+var service_url = authenticate.translateConfig.service_url;
+var service_username = authenticate.translateConfig.service_username;
+var service_password = authenticate.translateConfig.service_password;
 
-// // VCAP_SERVICES contains all the credentials of services bound to
-// // this application. For details of its content, please refer to
-// // the document or sample of each service.
-// if (process.env.VCAP_SERVICES) {
-//   console.log('Parsing VCAP_SERVICES');
-//   var services = JSON.parse(process.env.VCAP_SERVICES);
-//   //service name, check the VCAP_SERVICES in bluemix to get the name of the services you have
-//   var service_name = 'machine_translation';
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+if (process.env.VCAP_SERVICES) {
+  console.log('Parsing VCAP_SERVICES');
+  var services = JSON.parse(process.env.VCAP_SERVICES);
+  //service name, check the VCAP_SERVICES in bluemix to get the name of the services you have
+  var service_name = 'machine_translation';
   
-//   if (services[service_name]) {
-//     var svc = services[service_name][0].credentials;
-//     service_url = svc.url;
-//     service_username = svc.username;
-//     service_password = svc.password;
-//   } else {
-//     console.log('The service '+service_name+' is not in the VCAP_SERVICES, did you forget to bind it?');
-//   }
+  if (services[service_name]) {
+    var svc = services[service_name][0].credentials;
+    service_url = svc.url;
+    service_username = svc.username;
+    service_password = svc.password;
+  } else {
+    console.log('The service '+service_name+' is not in the VCAP_SERVICES, did you forget to bind it?');
+  }
 
-// } else {
-//   console.log('No VCAP_SERVICES found in ENV, using defaults for local development');
-// }
+} else {
+  console.log('No VCAP_SERVICES found in ENV, using defaults for local development');
+}
 
-// console.log('service_url = ' + service_url);
-// console.log('service_username = ' + service_username);
-// console.log('service_password = ' + new Array(service_password.length).join("X"));
+console.log('service_url = ' + service_url);
+console.log('service_username = ' + service_username);
+console.log('service_password = ' + new Array(service_password.length).join("X"));
 
-// var auth = 'Basic ' + new Buffer(service_username + ':' + service_password).toString('base64');
+var auth = 'Basic ' + new Buffer(service_username + ':' + service_password).toString('base64');
 
-//   var request_data = { 
-//     'txt': req.body.text, 
-//     'sid': req.body.sid,
-//     'rt':'text' // return type e.g. json, text or xml
-//   };
+  var request_data = { 
+    'txt': req.body.text, 
+    'sid': req.body.sid,
+    'rt':'text' // return type e.g. json, text or xml
+  };
   
-//   var parts = url.parse(service_url);
-//   // create the request options to POST our question to Watson
-//   var options = { host: parts.hostname,
-//     port: parts.port,
-//     path: parts.pathname,
-//     method: 'POST',
-//     headers: {
-//       'Content-Type'  :'application/x-www-form-urlencoded', // only content type supported
-//       'X-synctimeout' : '30',
-//       'Authorization' :  auth }
-//   };
+  var parts = url.parse(service_url);
+  // create the request options to POST our question to Watson
+  var options = { host: parts.hostname,
+    port: parts.port,
+    path: parts.pathname,
+    method: 'POST',
+    headers: {
+      'Content-Type'  :'application/x-www-form-urlencoded', // only content type supported
+      'X-synctimeout' : '30',
+      'Authorization' :  auth }
+  };
 
-//   // Create a request to POST to Watson
-//   var watson_req = https.request(options, function(result) {
-//     result.setEncoding('utf-8');
-//     var responseString = '';
+  // Create a request to POST to Watson
+  var watson_req = https.request(options, function(result) {
+    result.setEncoding('utf-8');
+    var responseString = '';
 
-//     result.on("data", function(chunk) {
-//       responseString += chunk;
-//     });
+    result.on("data", function(chunk) {
+      responseString += chunk;
+    });
     
-//     result.on('end', function() {
-//       // add the response to the request so we can show the text and the response in the template
-//       request_data.translation = responseString;
-//       console.log('request',request_data);
-//       callback(request_data);
-//     })
+    result.on('end', function() {
+      // add the response to the request so we can show the text and the response in the template
+      request_data.translation = responseString;
+      console.log('request',request_data);
+      callback(request_data);
+    })
 
-//   });
+  });
 
-//   watson_req.on('error', function(e) {
-//     return res.render('index', {'error': e.message})
-//   });
+  watson_req.on('error', function(e) {
+    return res.render('index', {'error': e.message})
+  });
 
-//   // create the request to Watson
-//   watson_req.write(querystring.stringify(request_data));
-//   watson_req.end();
+  // create the request to Watson
+  watson_req.write(querystring.stringify(request_data));
+  watson_req.end();
 
 }
 
